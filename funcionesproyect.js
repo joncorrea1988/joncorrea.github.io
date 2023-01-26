19c5b855b1c2f3b3ef51935ab8619c121f1f9685
joncorrea1988 <123401375+joncorrea1988@users.noreply.github.com>
2023-01-26 11:48:38 -0300

    primer

---
+++ funcionesproyect.js
@@ -0,0 +1,11 @@
+document.getElementById("botom2").onclick = function (){
+
+
+document.getElementById("Referencias").style.display="none";
+}
+
+document.getElementById("botom").onclick = function (){
+
+    document.getElementById("Referencias").style.display="block"
+
+}
